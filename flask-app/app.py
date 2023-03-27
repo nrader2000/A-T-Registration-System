121254bb26284fb459a82dcf3b8fd9ee06aaf967
Nick Rader <69931749+nrader2000@users.noreply.github.com>
2023-03-27 18:34:59 -0400

    created general UI templates

---
+++ flask-app/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/class-registration")
+def CR():
+    return render_template("CR.html")
+
+@app.route("/academic-audit")
+def AA():
+    return render_template("AA.html")
+
+@app.route("/course-override-request")
+def COR():
+    return render_template("COR.html")
+
+@app.route("/view-available-classes")
+def VAC():
+    return render_template("VAC.html")
+
+@app.route("/view-suggested-semester-classes")
+def VSSC():
+    return render_template("VSSC.html")
+
+@app.route("/view-grades")
+def VG():
+    return render_template("VG.html")
+
+@app.route("/view-transcript-information")
+def VTI():
+    return render_template("VTI.html")
+
+@app.route("/view-student-information")
+def VSI():
+    return render_template("VSI.html")
+
+@app.route("/register-for-graduation")
+def RFG():
+    return render_template("RFG.html")
